65a6d0750c8bb1c8bdafe2ebb22ef1ce3b989841
pandya.manav@gmail.com <pandya.manav@gmail.com>
2017-11-23 12:13:03 +0530

    Initial Commit

---
+++ CRUD-Book-Unity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRUD_Book_Unity.Startup))]
+namespace CRUD_Book_Unity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
